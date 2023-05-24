cb35196db9e6cf6a04d25b7243a3ed84a26106e0
Amritha Dinesh <adinesh@ftdi.com>
2023-05-24 14:34:59 +0530

    Added the project structure
    Added the project structure

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <View>
+      <Text>Hello world</Text>
+    </View>
+  );
+}
+
+export default App;
