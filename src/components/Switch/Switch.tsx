df9f487db7ca8b9a85e228ea6e0e4a8a3077a16a
Shashikant <patilshashi43@gmail.com>
2023-05-31 14:33:08 +0530

    Removed unwanted code and added descriptions

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -37,44 +37,6 @@ export type Props = React.ComponentPropsWithRef<typeof NativeSwitch> & {
   accessibilityLabel?: string;
 };
 
-/**
- * Switch is a visual toggle between two mutually exclusive states — on and off.
- *
- * <div class="screenshots">
- *   <figure>
- *     <img src="screenshots/switch-enabled.android.png" />
- *     <figcaption>Android (enabled)</figcaption>
- *   </figure>
- *   <figure>
- *     <img src="screenshots/switch-disabled.android.png" />
- *     <figcaption>Android (disabled)</figcaption>
- *   </figure>
- *   <figure>
- *     <img src="screenshots/switch-enabled.ios.png" />
- *     <figcaption>iOS (enabled)</figcaption>
- *   </figure>
- *   <figure>
- *     <img src="screenshots/switch-disabled.ios.png" />
- *     <figcaption>iOS (disabled)</figcaption>
- *   </figure>
- * </div>
- *
- * ## Usage
- * ```js
- * import * as React from 'react';
- * import { Switch } from 'react-native-paper';
- *
- * const MyComponent = () => {
- *   const [isSwitchOn, setIsSwitchOn] = React.useState(false);
- *
- *   const onToggleSwitch = () => setIsSwitchOn(!isSwitchOn);
- *
- *   return <Switch value={isSwitchOn} onValueChange={onToggleSwitch} />;
- * };
- *
- * export default MyComponent;
- * ```
- */
 const Switch = ({ value, disabled, onValueChange, color, ...rest }: Props) => {
   const { checkedColor, onTintColor, thumbTintColor } = getSwitchColor({
     disabled,
