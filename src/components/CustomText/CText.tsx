852666427d61add393d4b201bf0e7ffc90af3b6c
manojsai_bellamkonda <manojsai.bellamkonda@bounteous.com>
2023-06-15 13:23:45 +0530

    added variant and textType props

--- src/components/CustomText/CText.tsx
+++ src/components/CustomText/CText.tsx
@@ -1,28 +1,176 @@
+/*
+If you want to visuvalise all the various texts possible, use below code
+#imports
+import {FlatList } from "react-native";
+import { CText } from "./mainExport";
+
+#variable with all types possible
+  const AllPossibleTypesOfText: any[] = [
+    {
+      variant: "headingLarge",
+      textType: "primaryBold",
+      content: "This is headingLarge & primaryBold",
+    },
+    {
+      variant: "headingNormal",
+      textType: "primaryNormal",
+      content: "This is headingNormal & primaryNormal",
+    },
+    {
+      variant: "heading",
+      textType: "primary",
+      content: "This is heading & primary",
+    },
+    {
+      variant: "headingSmall",
+      textType: "primaryLight",
+      content: "This is headingSmall & primaryLight",
+    },
+
+    {
+      variant: "subHeadingLarge",
+      textType: "secondaryBold",
+      content: "This is subHeadingLarge & secondaryBold",
+    },
+    {
+      variant: "subHeadingNormal",
+      textType: "secondaryNormal",
+      content: "This is subHeadingNormal & secondaryNormal",
+    },
+    {
+      variant: "subHeading",
+      textType: "secondary",
+      content: "This is subHeading & secondary",
+    },
+    {
+      variant: "subHeadingSmall",
+      textType: "secondaryLight",
+      content: "This is subHeadingSmall & secondaryLight",
+    },
+
+    {
+      variant: "bodyLarge",
+      textType: "successBold",
+      content: "This is bodyLarge & successBold",
+    },
+    {
+      variant: "bodyNormal",
+      textType: "successNormal",
+      content: "This is bodyNormal & successNormal",
+    },
+    {
+      variant: "body",
+      textType: "success",
+      content: "This is body & success",
+    },
+    {
+      variant: "bodySmall",
+      textType: "successLight",
+      content: "This is bodySmall & successLight",
+    },
+
+    {
+      textType: "errorBold",
+      content: "This is errorBold",
+    },
+    {
+      textType: "errorNormal",
+      content: "This is errorNormal",
+    },
+    {
+      textType: "error",
+      content: "This is error",
+    },
+    {
+      textType: "errorLight",
+      content: "This is errorLight",
+    },
+
+    {
+      textType: "warningBold",
+      content: "This is warningBold",
+    },
+    {
+      textType: "warningNormal",
+      content: "This is warningNormal",
+    },
+    {
+      textType: "warning",
+      content: "This is warning",
+    },
+    {
+      textType: "warningLight",
+      content: "This is warningLight",
+    },
+    {
+      textType: "bold",
+      content: "This is bold",
+    },
+    {
+      textType: "normal",
+      content: "This is normal",
+    },
+    {
+      textType: "light",
+      content: "This is light",
+    },
+  ];
+
+#FlatList to render all those types
+ <FlatList
+        data={AllPossibleTypesOfText}
+        renderItem={({ item }) => (
+          <CText variant={item?.variant} textType={item?.textType}>
+            {item.content}
+          </CText>
+        )}
+      />
+
+*/
+
 import React, { ReactNode } from "react";
-import { Text } from "react-native";
+import { Text, TextStyle } from "react-native";
+import { variantTypes, textTypes } from "./utils";
 
 export type CTextTypes = {
   children: ReactNode;
   accessibilityLabel?: string;
   testID?: string;
-  style?: { [key: string]: any };
+  style?: TextStyle;
+  variant?: string;
+  textType?: string;
 };
 
+//TODO: need to add comments for each prop
 const CText: React.FC<CTextTypes> = ({
   children,
   accessibilityLabel,
   testID,
   style,
+  variant,
+  textType,
   ...rest
-}) => (
-  <Text
-    accessibilityLabel={accessibilityLabel}
-    testID={testID}
-    style={style}
-    {...rest}
-  >
-    {children}
-  </Text>
-);
+}) => {
+  const variantstyles: TextStyle =
+    variant && variantTypes.hasOwnProperty(variant)
+      ? variantTypes[variant as keyof typeof variantTypes]
+      : variantTypes.body;
+
+  const textTypeStyles: TextStyle =
+    textType && textTypes.hasOwnProperty(textType)
+      ? textTypes[textType as keyof typeof textTypes]
+      : textTypes.normal;
+
+  return (
+    <Text
+      accessibilityLabel={accessibilityLabel}
+      testID={testID}
+      style={[variantstyles, textTypeStyles, style]}
+      {...rest}
+    >
+      {children}
+    </Text>
+  );
+};
 
 export default CText;
---
+++ src/components/CustomText/utils.tsx
@@ -0,0 +1,156 @@
+import { TextStyle } from "react-native";
+//TODO: need define fonts and add font-family to these variants
+
+export const variantTypes: { [key: string]: TextStyle } = {
+  headingLarge: {
+    fontSize: 30,
+    fontWeight: "bold",
+    lineHeight: 36,
+    letterSpacing: 0,
+  },
+  headingNormal: {
+    fontSize: 26,
+    fontWeight: "bold",
+    lineHeight: 32,
+    letterSpacing: 0,
+  },
+  headingSmall: {
+    fontSize: 22,
+    fontWeight: "bold",
+    lineHeight: 28,
+    letterSpacing: 0,
+  },
+  heading: {
+    fontSize: 26,
+    fontWeight: "bold",
+    lineHeight: 32,
+    letterSpacing: 0,
+  },
+
+  subHeadingLarge: {
+    fontSize: 24,
+    fontWeight: "normal",
+    lineHeight: 28,
+    letterSpacing: 0,
+  },
+  subHeadingNormal: {
+    fontSize: 22,
+    fontWeight: "normal",
+    lineHeight: 26,
+    letterSpacing: 0,
+  },
+  subHeadingSmall: {
+    fontSize: 20,
+    fontWeight: "normal",
+    lineHeight: 24,
+    letterSpacing: 0,
+  },
+  subHeading: {
+    fontSize: 22,
+    fontWeight: "normal",
+    lineHeight: 26,
+    letterSpacing: 0,
+  },
+
+  bodyLarge: {
+    fontSize: 20,
+    fontWeight: "normal",
+    lineHeight: 26,
+    letterSpacing: 0,
+  },
+  bodyNormal: {
+    fontSize: 18,
+    fontWeight: "normal",
+    lineHeight: 22,
+    letterSpacing: 0,
+  },
+  bodySmall: {
+    fontSize: 16,
+    fontWeight: "normal",
+    lineHeight: 20,
+    letterSpacing: 0,
+  },
+  body: {
+    fontSize: 18,
+    fontWeight: "normal",
+    lineHeight: 22,
+    letterSpacing: 0,
+  },
+};
+
+export const textTypes: { [key: string]: TextStyle } = {
+  primaryLight: {
+    color: "#B3D4FF",
+  },
+  primaryNormal: {
+    color: "#007BFF",
+  },
+  primaryBold: {
+    color: "#0047B2",
+  },
+  primary: {
+    color: "#007BFF",
+  },
+
+  secondaryLight: {
+    color: "#A6B1B9",
+  },
+  secondaryNormal: {
+    color: "#6C757D",
+  },
+  secondaryBold: {
+    color: "#465057",
+  },
+  secondary: {
+    color: "#6C757D",
+  },
+
+  successLight: {
+    color: "#8EDFAF",
+  },
+  successNormal: {
+    color: "#28A745",
+  },
+  successBold: {
+    color: "#1E8435",
+  },
+  success: {
+    color: "#28A745",
+  },
+
+  errorLight: {
+    color: "#F1A8AF",
+  },
+  errorNormal: {
+    color: "#DC3545",
+  },
+  errorBold: {
+    color: "#A32939",
+  },
+  error: {
+    color: "#DC3545",
+  },
+
+  warningLight: {
+    color: "#FFE383",
+  },
+  warningNormal: {
+    color: "#FFC107",
+  },
+  warningBold: {
+    color: "#E0A800",
+  },
+  warning: {
+    color: "#FFC107",
+  },
+
+  light: {
+    color: "#666666",
+  },
+  normal: {
+    color: "#333333",
+  },
+  bold: {
+    color: "#000000",
+  },
+};
