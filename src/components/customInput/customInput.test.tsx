70b4780d3110c920b92fe7dcd005e3ab4b34883f
manojsai_bellamkonda <manojsai.bellamkonda@bounteous.com>
2023-06-28 20:13:43 +0530

    updated test cases

--- src/components/customInput/customInput.test.tsx
+++ src/components/customInput/customInput.test.tsx
@@ -23,36 +23,35 @@ describe("CustomInput Component", () => {
 
   it("renders custom input mode by default", () => {
     const { getByTestId } = render(<CustomInput />);
-    const filledStyle = [
-      { backgroundColor: "transparent", borderBottomWidth: 1 },
-      { borderColor: "rgb(28, 28, 30)", color: "rgb(28, 28, 30)" },
-    ];
+    const filledStyle = {
+      borderColor: "rgb(28, 28, 30)",
+      color: "rgb(28, 28, 30)",
+      backgroundColor: "rgb(242, 242, 242)",
+    };
     const customInputComponent = getByTestId("custom-input");
-    expect(customInputComponent.props.style[2]).toEqual(filledStyle);
+    expect(customInputComponent.props.style[1]).toEqual(filledStyle);
   });
 
   it("renders disabled custom input when mode prop is outlined", () => {
     const { getByTestId } = render(<CustomInput mode="outlined" />);
-    const outlinedStyle = [
-      { backgroundColor: "transparent", borderWidth: 1 },
-      {
-        borderRadius: 5,
-        borderColor: "rgb(28, 28, 30)",
-        color: "rgb(28, 28, 30)",
-      },
-    ];
+    const outlinedStyle = {
+      backgroundColor: "rgb(242, 242, 242)",
+      borderColor: "rgb(28, 28, 30)",
+      color: "rgb(28, 28, 30)",
+    };
     const customInputComponent = getByTestId("custom-input");
-    expect(customInputComponent.props.style[2]).toEqual(outlinedStyle);
+    expect(customInputComponent.props.style[1]).toEqual(outlinedStyle);
   });
 
   it("renders disabled custom input when mode prop is standard", () => {
     const { getByTestId } = render(<CustomInput mode="standard" />);
-    const standardStyle = [
-      { backgroundColor: "transparent", borderBottomWidth: 1 },
-      { borderColor: "rgb(28, 28, 30)", color: "rgb(28, 28, 30)" },
-    ];
+    const standardStyle = {
+      backgroundColor: "rgb(242, 242, 242)",
+      borderColor: "rgb(28, 28, 30)",
+      color: "rgb(28, 28, 30)",
+    };
     const customInputComponent = getByTestId("custom-input");
-    expect(customInputComponent.props.style[2]).toEqual(standardStyle);
+    expect(customInputComponent.props.style[1]).toEqual(standardStyle);
   });
 
   it("renders enabled custom input by default", () => {
